e46eb5a52b02f3b2fb9439160636b981461d386e
Rohit Nair <rohitnair987@gmail.com>
2021-01-05 20:24:53 -0800

    Sample textbox to test api calls (#2)
    * Project setup; Updated readme
    
    * Sample textbox to test api calls

--- cric-exchange/src/App.js
+++ cric-exchange/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { getPlayerByPlayerId } from './api/getPlayers';
 
 const App = () => {
+    const [query, setQuery] = useState('');
+    const [player, setPlayer] = useState({});
+
+    const search = async (e) => {
+        if(e.key === 'Enter') {
+            const data = await getPlayerByPlayerId(query);
+            console.log(data);
+
+            setPlayer(data);
+            setQuery('');
+        }
+    }
+    
     return (
-        <h1>Sample text</h1>
-    )
+        <div className="main-container">
+            <input type="text"
+                className="search"
+                placeholder="Search..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+            />
+
+            {player.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{player.name}</span>
+                    </h2>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
